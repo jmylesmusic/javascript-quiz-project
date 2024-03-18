b154ba8a97def5790c7ebb5701fbe0e43a849901
James Myles <myles.james@gmail.com>
2024-03-18 16:24:27 +0100

    Day One

--- src/question.js
+++ src/question.js
@@ -2,6 +2,24 @@ class Question {
     // YOUR CODE HERE:
     //
     // 1. constructor (text, choices, answer, difficulty)
-
+constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+}
     // 2. shuffleChoices()
+    shuffleChoices() {
+        let input = this.choices;
+
+        for (let i = input.length - 1; i >= 0; i--) {
+      
+          let randomIndex = Math.floor(Math.random() * (i + 1));
+          let itemAtIndex = input[randomIndex];
+      
+          input[randomIndex] = input[i];
+          input[i] = itemAtIndex;
+        }
+        return input;
+    }
 }
